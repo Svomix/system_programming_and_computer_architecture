84e6dbf3e73de285b70a893b55d0c7870c06aeb3
Igor <igorvlasov732@gmail.com>
2025-03-18 18:53:07 +0400

    first pass

---
+++ lab2.cpp
@@ -0,0 +1,91 @@
+﻿#include <iostream>
+
+
+int f_cpp(int a, int b, int c)
+{
+    int divisible = 2 * b - a + b * c;
+    int divider = c / 4 - 1;
+    if (divider)
+    {
+        return divisible / divider;
+    }
+    std::cout << "CPP: Dividing by 0\n";
+    return 0;
+}
+
+int input_int()
+{
+    int res = 0;
+    while (!(std::cin >> res) || std::cin.peek() != '\n')
+    {
+        std::cin.clear();
+        while (std::cin.get() != '\n');
+        std::cout << "Incorrect input\n";
+        std::cout << "Enter the correct num\n";
+    }
+    return res;
+}
+
+int f_asm(int a, int b, int c)
+{
+    int result = 0;
+    bool div_zer = 0, overflow = 0;
+    __asm
+    {
+        mov eax, c;    eax = c
+        mov ebx, 4;    ebx = 4, eax = c
+        cdq;      eax = c, ebx = 4
+        idiv ebx;    eax = c / 4, ebx = 4
+        dec eax;    eax = c / 4 - 1, ebx = 4
+        jz zero_label;  eax = c / 4 - 1, ebx = 4
+        push eax;    eax = c / 4 - 1, ebx = 4
+        mov eax, b;    eax = b, ebx = 4
+        mov ebx, 2;    eax = b, ebx = 2
+        imul ebx;       eax = 2 * b, ebx = 2
+        jo of_label;    jump to of_label, eax = 2 * b, ebx = 2
+        mov ebx, a;     ebx = a, eax = 2 * b
+        sub eax, ebx;   eax = 2 * b - a, ebx = a
+        jo of_label;    jump to of_label, eax = eax - ebx, ebx = a
+        mov ebx, eax;   ebx = 2 * b - a, eax = 2 * b - a
+        mov ecx, c;     ecx = c, ebx = 2 * b - a, eax = 2 * b - a
+        mov eax, b;     eax = b, ecx = c, ebx = 2 * b - a
+        imul ecx;       eax = c * b, ecx = c, ebx = 2 * b - a
+        jo of_label;    jump to of_label
+        add eax, ebx;   eax = 2 * b - a + c * b, ecx = c, ebx = 2 * b - a
+        jo of_label;    jump to of_label
+        pop ebx;        ebx = c / 4 - 1, eax = 2 * b - a + c * b, ecx = c
+        cdq;            ebx = c / 4 - 1, eax = 2 * b - a + c * b, ecx = c
+        idiv ebx;       eax = (2 * b - a + c * b) / (c / 4 - 1), ebx = c / 4 - 1, ecx = c
+        mov result, eax;result = (2 * b - a + c * b) / (c / 4 - 1), eax = (2 * b - a + c * b) / (c / 4 - 1), ebx = c / 4 - 1, ecx = c
+        jmp end_label;  end of the asm, eax = (2 * b - a + c * b) / (c / 4 - 1), ebx = c / 4 - 1, ecx = c
+        zero_label :
+        mov div_zer, 1; div_zer = 1
+            jmp end_label;  end of the asm
+            of_label :
+        mov overflow, 1; overflow = 1
+            pop eax; get stack
+            jmp end_label; end of the asm
+            end_label :
+    }
+    if (div_zer)
+        std::cout << "ASM Error: dividing by 0\n";
+    if (overflow)
+        std::cout << "ASM Error: overflow\n";
+    return result;
+}
+
+int main()
+{
+    int a, b, c;
+    while (true)
+    {
+        std::cout << "Enter a:\n";
+        a = input_int();
+        std::cout << "Enter b:\n";
+        b = input_int();
+        std::cout << "Enter c:\n";
+        c = input_int();
+        std::cout << "The result of CPP\n" << f_cpp(a, b, c) << std::endl;
+        std::cout << "The result of ASM\n" << f_asm(a, b, c) << std::endl;
+    }
+}
---
+++ lab3.cpp
@@ -0,0 +1,126 @@
+﻿#include <iostream>
+
+void output(int x)
+{
+    std::cout << x << std::endl;
+}
+
+void output(int x, bool div_zero, bool overflow)
+{
+    if (div_zero)
+        std::cout << "ASM Error: dividing by zero\n";
+    if (overflow)
+        std::cout << "ASM Error: overflow\n";
+    std::cout << x << std::endl;
+}
+
+int input()
+{
+    int res = 0;
+    while (!(std::cin >> res) || std::cin.peek() != '\n')
+    {
+        std::cin.clear();
+        while (std::cin.get() != '\n');
+        std::cout << "Incorrect input\n";
+        std::cout << "Enter the num\n";
+    }
+    return res;
+}
+
+int cpp_func(int a, int b)
+{
+    int x;
+    if (a > b)
+    {
+        int divider = b + 5;
+        if (!divider)
+        {
+            std::cout << "CPP Error: dividing by zero\n";
+            return 0;
+        }
+        x = a / divider - 10;
+        return x;
+    }
+    else if (a == b)
+    {
+        x = 51;
+        return x;
+    }
+    if (a == 0)
+    {
+        std::cout << "CPP Error: dividing by zero\n";
+        return 0;
+    }
+    x = (a * b - 14 * b) / a;
+    return x;
+}
+
+int asm_func(int a, int b)
+{
+    bool div_zero = 0;
+    bool overflow = 0;
+    int x = 0;
+    __asm
+    {
+        mov ecx, a;     ecx = a
+        mov ebx, b;     ebx = b, ecx = a
+        cmp ecx, ebx;   ebx = b, ecx = a
+        jl smaller;     ebx = b, ecx = a
+        jg greater;     ebx = b, ecx = a
+        mov eax, 51;    eax = 51, ebx = b, ecx = a
+        jmp save_res
+        smaller :
+        cmp a, 0;       ebx = b, ecx = a
+            jz dividing_zero
+            imul ecx, ebx; ecx = a * b, ebx = b
+            jo of; ebx;    ecx = a * b, ebx = b
+            imul ebx, 14;  ebx = b * 14, ecx = a * b
+            jo of;          ebx = b * 14, ecx = a * b
+            sub ecx, ebx;  ecx = a * b - b * 14
+            jo of;
+        mov eax, ecx;       eax = a * b - b * 14, ecx = a * b - b * 14, ebx = b * 14
+            mov ecx, a;     ecx = a, eax = a * b - b * 14, ebx = b * 14
+            cdq;            ecx = a, eax = a * b - b * 14, ebx = b * 14
+            idiv ecx;       eax = (a * b - b * 14) / a
+            jmp save_res;   eax = (a * b - b * 14) / a
+            greater :
+        add ebx, 5;         ebx = b + 5, ecx = a
+            jo of;          ebx = b + 5, ecx = a
+            jz dividing_zero; ebx = b + 5, ecx = a
+            mov eax, ecx;       eax = a, ebx = b + 5, ecx = a
+            cdq;                eax = a, ebx = b + 5
+            idiv ebx;       eax = a / (b + 5), ebx = b + 5, ecx = a
+            sub eax, 10;    eax = a / (b + 5) - 10, ebx = b + 5
+            jo of;          eax = a / (b + 5) - 10, ebx = b + 5
+            jmp save_res
+            dividing_zero :
+        mov div_zero, 1
+            jmp exit_asm
+            save_res :
+        mov x, eax;         x = eax
+            jmp exit_asm
+            of :
+        mov overflow, 1
+            jmp exit_asm
+            exit_asm :
+    }
+    output(x, div_zero, overflow);
+}
+
+
+int main()
+{
+    int a;
+    int b;
+    while (true)
+    {
+        std::cout << "Input a:\n";
+        a = input();
+        std::cout << "Input b:\n";
+        b = input();
+        std::cout << "CPP result:\n";
+        output(cpp_func(a, b));
+        std::cout << "ASM result:\n";
+        asm_func(a, b);
+    }
+}
